3b1ad82e62db59dfe45992d4bb3df3b8ca2cf175
Larry Crochet <larry.crochet@libertyits.com>
2020-07-01 20:28:16 -0400

    maven invoker, tar items

--- unifier-tests/src/test/java/gov/va/api/health/conformance/unifier/test/scriptedtest/ScriptedDockerTests.java
+++ unifier-tests/src/test/java/gov/va/api/health/conformance/unifier/test/scriptedtest/ScriptedDockerTests.java
@@ -3,12 +3,14 @@ package gov.va.api.health.conformance.unifier.test.scriptedtest;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DockerClientBuilder;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
+import java.io.InputStream;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.DefaultInvoker;
@@ -17,16 +19,41 @@ import org.apache.maven.shared.invoker.InvocationResult;
 import org.apache.maven.shared.invoker.Invoker;
 import org.apache.maven.shared.invoker.MavenInvocationException;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(ScriptedTest.class)
 public class ScriptedDockerTests {
+  private static File localRepositoryDir;
+
+  private static DockerClient dockerClient;
 
   private Invoker invoker;
 
-  private File localRepositoryDir = new File(System.getProperty("user.home") + ".m2/repository");
-  private DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+  @BeforeClass
+  public static void initialize() {
+    String repoPath = System.getProperty("user.home") + "/.m2/repository";
+    localRepositoryDir = Paths.get(repoPath).toFile();
+    dockerClient = DockerClientBuilder.getInstance().build();
+  }
+
+  public static void untar(InputStream tarIn, File out) throws IOException {
+    try (TarArchiveInputStream fin = new TarArchiveInputStream(tarIn)) {
+      TarArchiveEntry entry;
+      while ((entry = fin.getNextTarEntry()) != null) {
+        if (entry.isDirectory()) {
+          continue;
+        }
+        File curfile = new File(out, entry.getName());
+        File parent = curfile.getParentFile();
+        if (!parent.exists()) {
+          parent.mkdirs();
+        }
+        IOUtils.copy(fin, new FileOutputStream(curfile));
+      }
+    }
+  }
 
   @Before
   public void init() {
@@ -46,26 +73,22 @@ public class ScriptedDockerTests {
   }
 
   @Test
-  public void mavenGoals() throws MavenInvocationException, IOException {
+  public void mavenScripts() throws MavenInvocationException, IOException {
     mavenGoals(Arrays.asList("-Plocaltest docker:start"));
     mavenGoals(
         Arrays.asList(
             "-Plocaltest -pl conformance-unifier -am spring-boot:run -Dspring-boot.run.arguments=\"dstu2,metadata,https://api.va.gov/services/fhir/v0/dstu2/metadata\""));
-
-    try (OutputStream os = Files.newOutputStream(Paths.get("./target/s3results.tar"))) {
-      IOUtils.copy(
-          dockerClient
-              .copyArchiveFromContainerCmd(
-                  "conformanceS3Mock", "/tmp/s3mockFileStore1593641003848/")
-              .exec(),
-          os);
-    }
-    //    mavenGoals(Arrays.asList("-Plocaltest docker:stop"));
+    untar(
+        dockerClient
+            .copyArchiveFromContainerCmd("conformanceS3Mock", "/tmp/s3mockFileStore1593641003848/")
+            .exec(),
+        Paths.get("./target/s3results").toFile());
+    // mavenGoals(Arrays.asList("-Plocaltest docker:stop"));
   }
 
   @Test
   public void showTestCategory() {
     System.out.println("============================> Scripted Tests");
-//    System.out.println(System.getProperties().toString());
+    // System.out.println(System.getProperties().toString());
   }
 }
